a0c19336c2c04bd96fd0b2a9d40c4310901a87aa
Crowdedlight <crowdedhd@gmail.com>
2015-04-09 19:37:13 +0200

    removed btnvalue line as not used and thowed an error

--- Average16bit/Average16bit.asm
+++ Average16bit/Average16bit.asm
@@ -4,13 +4,6 @@
 
 SETUP:
 
-    ; PORTC setup
-
-    ldi				R16,		0x00				; Read input into R16
-    out				DDRC,		R16					; Set PORTC as input
-    ldi				R16,		255					; Set Pull-up value in R16 (Max value in decimal)
-    out 			PORTC,   	R16 				; Enable pull-up on PORTC
-
     ; PORTB setup
 
     ldi				R16,		0xFF				; Set output value in R16
@@ -44,7 +37,6 @@ SETUP:
 ; Clearing Register
     clr         QuotMSB
     clr         QuotLSB
-    clr         BtnValue
     clr         Denominator
     clr         SecondNumLSB
     clr         SecondNumMSB
